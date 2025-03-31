4f9ba1f26946322cdc78b596e50c2e4ec3c15892
Antonio De Lucreziis <antonio.delucreziis@gmail.com>
2025-04-01 01:25:04 +0200

    initial prototype

---
+++ README.md
@@ -0,0 +1,15 @@
+# 2025-03-31-data-notebook
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun run index.ts
+```
+
+This project was created using `bun init` in bun v1.2.6. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
---
+++ src/main.jsx
@@ -0,0 +1,161 @@
+import clsx from 'clsx'
+import { render } from 'preact'
+
+const DataValueRenderer = ({ value }) => {
+    if (value === null || value === undefined) {
+        return {
+            element: <div class="data null">null</div>,
+            depth: 0,
+        }
+    }
+    if (Array.isArray(value)) {
+        const items = value.map(item => {
+            const { element, depth } = DataValueRenderer({ value: item })
+            return {
+                element,
+                depth,
+            }
+        })
+
+        const depth = Math.max(...items.map(item => item.depth))
+
+        return {
+            element: (
+                <div class={clsx('data', 'array', { compact: depth < 1 })}>
+                    {value.map((item, index) => (
+                        <div class="data item" key={index}>
+                            <div class="data index">{index}</div>
+                            <div class="data value">{items[index].element}</div>
+                        </div>
+                    ))}
+                </div>
+            ),
+            depth,
+        }
+    }
+    if (typeof value === 'object') {
+        const entries = Object.entries(value).map(([key, val]) => {
+            const { element, depth } = DataValueRenderer({ value: val })
+            return {
+                key,
+                element,
+                depth,
+            }
+        })
+
+        return {
+            element: (
+                <div class="data object">
+                    {entries.map(({ key, element }, index) => (
+                        <div class="data entry" key={index}>
+                            <div class="data key">{key}</div>
+                            <div class="data value">{element}</div>
+                        </div>
+                    ))}
+                </div>
+            ),
+            depth: Math.max(...entries.map(item => item.depth)) + 1,
+        }
+    }
+    if (typeof value === 'string') {
+        return {
+            element: <div class="data string">{value}</div>,
+            depth: 0,
+        }
+    }
+    if (typeof value === 'number') {
+        return {
+            element: <div class="data number">{value}</div>,
+            depth: 0,
+        }
+    }
+    if (typeof value === 'boolean') {
+        return {
+            element: <div class="data boolean">{value ? 'true' : 'false'}</div>,
+            depth: 0,
+        }
+    }
+
+    throw new Error(`Unsupported data type: ${typeof value}`)
+}
+
+const Cell = ({ input, setInput, output, metadata }) => {
+    const { element: outputElement, depth } = DataValueRenderer({ value: output })
+
+    return (
+        <div class="cell">
+            <div class="input">
+                <div class="metadata-left">{metadata}</div>
+                <div class="content">
+                    <textarea
+                        value={input}
+                        onInput={e => setInput(e.target.value)}
+                        placeholder="Type something..."
+                        rows={Math.max(1, input.split('\n').length)}
+                    />
+                </div>
+            </div>
+            <div class="output">{outputElement}</div>
+        </div>
+    )
+}
+
+const Notebook = () => {
+    const cells = [
+        {
+            input: '[1 2 3]',
+            output: [1, 2, 3],
+        },
+        {
+            input: '[4 5 6]',
+            output: [4, 5, 6],
+        },
+        {
+            input: `
+                [
+                    { foo: 1, bar: 2 }
+                    { foo: 3, bar: 4 }
+                    { foo: 5, bar: 6 }
+                ]
+            `
+                .replace(/\s+/g, ' ')
+                .trim(),
+            output: [
+                { foo: 1, bar: 2 },
+                { foo: 3, bar: 4 },
+                { foo: 5, bar: 6 },
+            ],
+        },
+    ]
+
+    return (
+        <div class="notebook">
+            {cells.map((cell, index) => (
+                <Cell
+                    key={index}
+                    input={cell.input}
+                    setInput={() => {}}
+                    output={cell.output}
+                    metadata={<code>${index + 1}</code>}
+                />
+            ))}
+        </div>
+    )
+}
+
+const App = () => {
+    return (
+        <>
+            <nav>
+                <div class="left">DataSheet</div>
+                <div class="center">???</div>
+                <div class="right">Login</div>
+            </nav>
+            <main>
+                <Notebook />
+            </main>
+        </>
+    )
+}
+
+render(<App />, document.body)
